4364ffc7efd69a0d85dab6cb2c0efd1d2f4bf612
prebm <benjaminpreisig@gmail.com>
2016-06-25 12:33:02 +0200

    Write Zone Information to File

--- get_sonos_ip.py
+++ get_sonos_ip.py
@@ -5,8 +5,12 @@
 
 import soco
 import re
+import codecs
 
 
 zone_list = list(soco.discover())
-for zone in zone_list:
-    print u"Player: {0} at IP: {1}".format(zone.player_name, zone)
+
+with codecs.open('discovered.csv', "w", "utf-8-sig") as the_file:
+    for zone in zone_list:
+        print u"Player: {0} at IP: {1}".format(zone.player_name, zone)
+        the_file.write(u"Player: {0},{1}\n".format(zone.player_name, zone))
