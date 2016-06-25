c1b5e680a3603177a9bd6c7dbc3a90c963127822
prebm <benjaminpreisig@gmail.com>
2016-06-25 12:35:05 +0200

    Create install file to cover all steps automatically

---
+++ install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+##### install prerequisites #####
+#: <<'END'
+printf "\n###\n Start update and upgrade \n###\n"
+sudo apt-get update
+#sudo apt-get -y upgrade
+
+printf "\n###\n install pip, lirc and python-lirc \n###\n"
+sudo apt-get install -y python-pip lirc python-lirc
+
+printf "\n###\n install soco \n###\n"
+sudo pip install soco
+
+##### kernel check and /boot/config.txt #####
+
+printf "\n###\n Check Kernel version and change /boot/config.txt \n###\n"
+
+# get kernel number by removing the point, e.g. 4.4 => 44
+VERSION=$(awk -F. '{print $1$2}' <<< $(uname -r))
+echo "Version $(uname -r)"
+
+# make 44 to 440 to be bigger than 318
+if [ $VERSION -gt 39 ] && [ $VERSION -lt 100 ]
+then
+	VERSION=$((VERSION * 10))
+fi
+
+# if Kernel ≥ 3.18 edit /boot/config.txt, else edit /etc/modules
+if [ $VERSION -ge 318 ]
+then
+	#echo "Greater: $VERSION"
+	# replace lines in file, create backup file
+	sudo sed -i_backup -e 's/#dtoverlay=lirc-rpi/dtoverlay=lirc-rpi/g'  /boot/config.txt
+	printf "\nEdited /boot/config.txt and created backup /boot/config.txt_backup\n"
+else
+	#echo "Smaller: $VERSION"
+	# create backup file and add lines
+	sudo cp /etc/modules /etc/modules_backup
+	sudo echo lirc_dev >> /etc/modules
+	sudo echo lirc_rpi >> /etc/modules
+	printf "\nEdited /etc/modules and created backup /etc/modules_backup\n"
+fi
+
+##### hardware.conf #####
+
+printf "\n###\n Edit hardware.conf \n###\n"
+
+# create backup and change lines
+sudo cp /etc/lirc/hardware.conf /etc/lirc/hardware.conf_backup
+sudo sed -i -e 's/LIRCD_ARGS=""/LIRCD_ARGS="--uinput"/g' /etc/lirc/hardware.conf
+sudo sed -i -e 's/DRIVER="UNCONFIGURED"/DRIVER="default"/g' /etc/lirc/hardware.conf
+sudo sed -i -e 's/DEVICE=""/DEVICE="\/dev\/lirc0"/g' /etc/lirc/hardware.conf
+sudo sed -i -e 's/MODULES=""/MODULES="lirc_rpi"/g' /etc/lirc/hardware.conf
+
+printf "\nEdited /etc/lirc/hardware.conf and created backup /etc/lirc/hardware.conf_backup\n"
+
+##### lirc #####
+
+printf "\n###\n Copy lirc configuration files \n###\n"
+
+DIR_PATH=${PWD}
+echo $DIR_PATH
+
+sudo cp /etc/lirc/lircd.conf /etc/lirc/lircd.conf_backup
+sudo cp /etc/lirc/lircrc /etc/lirc/lircrc_backup
+
+sudo cp "$DIR_PATH/lircd.conf" /etc/lirc/
+sudo cp "$DIR_PATH/lircrc" /etc/lirc/
+
+printf "\nCreated backups and edited files in /etc/lirc/ \n"
+#END
+##### specific setup, IP #####
+
+# write Sonos Zones to file and read the file
+python get_sonos_ip.py
+
+printf "\n#####\nChoose the zone, enter the number and press [ENTER]: \n"
+J=1
+while IFS=, read name ip
+do
+	echo "$J. $name"
+	IP_LIST[$J]=${ip:19:-1}
+	J=$(($J + 1))
+done < discovered.csv
+
+# waiting for input
+read ZONE
+
+# replace string in config.py
+sudo sed -i -e "s/IP_ADDRESS='.*'/IP_ADDRESS='${IP_LIST[$ZONE]}'/g" "$DIR_PATH/config.py"
+
+printf "\nSaved chosen zone in $DIR_PATH/config.py \n"
+
+printf "\n#####\nRun sore.py to if it is working. \n"
+
+##### Enable daemon #####
+
+# TODO
